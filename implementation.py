db690e2c88dc759a5f4fafcbc03889a02c299419
Marc-Antoine Ourradour <83420479+Marco75116@users.noreply.github.com>
2022-01-06 04:27:15 +0100

    Add files via upload

--- implementation.py
+++ implementation.py
@@ -23,28 +23,35 @@ def encryption_machine(msg,key):
 
 
 
-def save_to_database(user, pwd , cipher , ciphertext):
+def save_to_database(user, pwd , cipher ):
     with open('encrypted.bin', 'wb') as f:
         f.write(cipher.iv)
-        f.write(ciphertext)
+        f.write(b"\n" + pwd)
+        f.write(b"\n" + user)
 
 def check_password(user, pwd,key):
+    lines=[]
     with open('encrypted.bin', 'rb') as f:
-        iv = f.read(16)
-        decrypt_data = f.read()
-
+        lines = f.readlines()
+    print(lines)
+    iv = lines[0].rstrip(b"\n")
+    decrypt_data = lines[1].rstrip(b"\n")
+    userdb = lines[2].rstrip(b"\n")
     cipher = AES.new(key, AES.MODE_CBC, iv=iv)
     original = unpad(cipher.decrypt(decrypt_data), AES.block_size)
-    return pwd == original
+    return pwd == original,user == userdb
 
 
 
+
+user = input("enter your username").encode()
+pwd = input("enter your password please").encode()
 
 key = generateKey("hehehe")
 #key =b'qx\xf3\xf8]\x9c \xabd\xd9\r\xe0\xea\xd8\xf4\xb0\x93/\xaa\x88\xac`y\x8b\xc2A\xfbs\x08p\xf7\x0e'
-cipher,ciphertext = encryption_machine(b"thisIsMyPassword",key)
-save_to_database("user", "pwd", cipher, ciphertext)
-if check_password("user", b"thisIsMyPassword",key) :
+cipher,ciphertext = encryption_machine(pwd,key)
+save_to_database(user, ciphertext, cipher, )
+if check_password( b"user" , b"thisIsMyPassword",key) :
     print("good authentication")
 else:
     print("ERROR , false username or password")
